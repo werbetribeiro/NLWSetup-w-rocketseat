f2104cd65ce5079a2b06a00e649ea2af76f26381
werbetribeiro <werbet.off@gmail.com>
2023-01-20 20:41:22 -0300

    ajuste no js

--- script.js
+++ script.js
@@ -22,13 +22,6 @@ function save() {
   localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))  
 }
 
-
-// const data = {
-//  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
-//  takePills: ["01-03"],
-//  journal: ["01-02"],
-// }
-
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
-// nlwSetup.setData(data)
-// nlwSetup.load()
+nlwSetup.setData(data)
+nlwSetup.load()
